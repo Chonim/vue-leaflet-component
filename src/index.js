cbf57b498b523a8d3c2880e3d5c53ee5d958efaa
Chonim <chihoyoon1217@gmail.com>
2018-10-23 15:31:18 +0900

    delete comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-// import Vue from 'vue'
 import VueLeafletComponent from './components/Map.vue'
 
 if (typeof window !== 'undefined' && window.Vue) {
   Vue.component('vue-leaflet-component', VueLeafletComponent)
 }
-
-/* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   components: {
-//     VueLeafletComponent
-//   },
-//   template: '<VueLeafletComponent/>'
-// })
 
 export { VueLeafletComponent }
 export default VueLeafletComponent
