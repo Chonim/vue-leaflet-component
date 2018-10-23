9f29d3383f2f92eb7f8ccbe765b8bd70203253b1
Chonim <chihoyoon1217@gmail.com>
2018-10-23 15:21:24 +0900

    comment out vue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import VueLeafletComponent from './components/Map'
+// import Vue from 'vue'
+import VueLeafletComponent from './components/Map.vue'
 
 if (typeof window !== 'undefined' && window.Vue) {
   Vue.component('vue-leaflet-component', VueLeafletComponent)
